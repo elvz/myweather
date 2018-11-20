98d6a7d8f00a15e2c2b21dc1cab55fdbffa74aff
elvz <djelvz@gmail.com>
2018-11-20 19:15:56 +0200

    Updates

---
+++ precache-manifest.4b8c2274d9e4d57eff11c72a99aad984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11365c5bb6b0c9a4ceaf",
+    "url": "/myweather/static/js/runtime~main.11365c5b.js"
+  },
+  {
+    "revision": "02d333f7ba81bfcb9c7c",
+    "url": "/myweather/static/js/main.02d333f7.chunk.js"
+  },
+  {
+    "revision": "ea09df903424c0b1611e",
+    "url": "/myweather/static/js/1.ea09df90.chunk.js"
+  },
+  {
+    "revision": "02d333f7ba81bfcb9c7c",
+    "url": "/myweather/static/css/main.7c85aa24.chunk.css"
+  },
+  {
+    "revision": "ea09df903424c0b1611e",
+    "url": "/myweather/static/css/1.9d0deda8.chunk.css"
+  },
+  {
+    "revision": "c7d2da1d4fbd6420d5dc49c9afaa2b33",
+    "url": "/myweather/index.html"
+  }
+];
